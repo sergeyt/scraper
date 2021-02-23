023d9c7531fe2746a3544d7d4fac8055de26c173
Sergey Todyshev <stodyshev@gmail.com>
2021-02-23 09:48:44 +0300

    scrap http://gender-decoder.katmatfield.com

---
+++ src/__tests__/gender-decoder.test.ts
@@ -0,0 +1,48 @@
+import _ from "lodash";
+import { unparse } from "papaparse";
+import { Source } from "../types";
+import { makeParser } from "../parse";
+
+const source: Source = {
+  type: "text",
+  name: "gender-decoder",
+  url: "http://gender-decoder.katmatfield.com",
+  makeUrl: () => "/about",
+  plan: [
+    {
+      selector: "#masculine li",
+      term: "masculine",
+      map: (s) => _.trimEnd(s, "-"),
+    },
+    {
+      selector: "#feminine li",
+      term: "feminine",
+      map: (s) => _.trimEnd(s, "-"),
+    },
+  ],
+};
+
+describe("gender-decoder words", () => {
+  it("extract", async (done) => {
+    const parser = makeParser(source);
+    const results = await parser({ text: "" });
+    const data = results[0].data;
+    const records = [];
+    const n = Math.max(data.masculine.length, data.feminine.length);
+    for (let i = 0; i < n; i++) {
+      const m = data.masculine[i];
+      const f = data.feminine[i];
+      const rec: any = {};
+      if (m) {
+        rec.masculine = m;
+      }
+      if (f) {
+        rec.feminine = f;
+      }
+      records.push(rec);
+    }
+    const csv = unparse(records);
+    console.log(csv);
+    done();
+  });
+});
