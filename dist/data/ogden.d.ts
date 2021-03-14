ffefba5b22f552de1d880a711cc89c09f93a8c48
Sergey Todyshev <stodyshev@gmail.com>
2021-03-14 15:05:46 +0300

    update build

--- dist/data/ogden.d.ts
+++ dist/data/ogden.d.ts
@@ -1,6 +1,12 @@
 export declare const ogden: {
     categories: {
         name: string;
-        words: string[];
+        words: ({
+            text: string;
+            freq: number;
+        } | {
+            text: string;
+            freq?: undefined;
+        })[];
     }[];
 };
