27e778f23aafec61468d127fb5ed3c3d67adde0f
Sergey Todyshev <stodyshev@gmail.com>
2023-01-15 21:06:20 +0300

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
   testMatch: ["**/__tests__/**/(*.)+(spec|test).[jt]s?(x)"],
   collectCoverage: false,
 };
